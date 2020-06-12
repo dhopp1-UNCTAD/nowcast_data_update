e144b7f22d4ab507c0dc7ef7613fd5390f8d6dd7
dhopp1 <daniel.hopp1@gmail.com>
2020-06-12 17:38:37 +0200

    More forecast testing

--- nowcast_targets/forecast_testing.r
+++ nowcast_targets/forecast_testing.r
@@ -30,7 +30,7 @@ data <- read_csv(paste0(db_directory, end_date, "_database_tf.csv")) %>%
 
 # gen forecast
 gen_forecast <- function (data, target, start_list, end_list) {
-  data <- data %>%
+  tmp <- data %>%
     window(start=start_list, end=end_list)
   if (target == "x_world.sa") {
     target_col <- "octave_value"  
@@ -52,7 +52,7 @@ gen_forecast <- function (data, target, start_list, end_list) {
     pull
   frequency <- lapply(frequency, function (r) if (r == "m") {12} else {4}) %>% unlist
   blocks <- cbind(as.matrix(rep(1, length(frequency))), as.matrix(rep(1, length(frequency))))
-  x <- data[,xs]
+  x <- tmp[,xs]
   
   x <- Bpanel(x, trans=rep(0, ncol(x)), na.prop=1, NA.replace=F)
   
@@ -88,3 +88,58 @@ while (as.Date(paste0(paste(which_list, collapse="-"), "-1")) <= end_date) {
   if (which_month == 1) {which_year <- which_list[1] + 1} else {which_year <- which_list[1]}
   which_list <- c(which_year, which_month)
 }
+
+# one ahead performance
+rmse <- function (pred, actual) { 
+  pred <- pred[!is.na(pred) & !is.na(actual)]
+  actual <- actual[!is.na(pred) & !is.na(actual)]
+  return (sqrt(mean((pred - actual)**2)))
+}
+month_diff <- function (month_1, month_2) {
+  tmp <- as.numeric(substr(month_2, 6, 7)) - as.numeric(substr(month_1, 6, 7))
+  if (tmp < 0) {tmp <- tmp + 12}
+  return (tmp)
+}
+
+# 2013-12, 2014-1, 2015-2 all predict 2015-3
+files <- list.files(path)
+perf <- data.frame(out=c(), date=c())
+for (file in files) {
+  tmp <- read_csv(paste0(path, file)) %>%
+    data.frame %>%
+    filter(date >= as.Date("2009-01-01"), !is.na(out)) %>%
+    slice(1:1) %>%
+    select(out, date) %>%
+    mutate(when_made = as.Date(paste0(str_split(file, ".csv")[[1]][1], "-01")))
+  perf <- rbind(perf, tmp)
+}
+
+months_ahead <- perf %>%
+  mutate(months_ahead = month_diff(when_made, date)) %>%
+  mutate(months_ahead = ifelse(months_ahead == -9, 3, months_ahead))
+
+metric_1_ahead <- read_csv(paste0(db_directory, end_date, "_database_tf.csv")) %>%
+  select(date, target) %>%
+  left_join(months_ahead %>% filter(months_ahead == 1) %>% select(out, date), by="date") %>% 
+  filter(!is.na(out)) %>%
+  rename(one_ahead=out)
+metric_2_ahead <- read_csv(paste0(db_directory, end_date, "_database_tf.csv")) %>%
+  select(date, target) %>%
+  left_join(months_ahead %>% filter(months_ahead == 2) %>% select(out, date), by="date") %>% 
+  filter(!is.na(out)) %>%
+  rename(two_ahead=out)
+metric_3_ahead <- read_csv(paste0(db_directory, end_date, "_database_tf.csv")) %>%
+  select(date, target) %>%
+  left_join(months_ahead %>% filter(months_ahead == 3) %>% select(out, date), by="date") %>% 
+  filter(!is.na(out)) %>%
+  rename(three_ahead=out)
+
+metric_final <- metric_1_ahead %>%
+  left_join(metric_2_ahead %>% select(date, two_ahead), by="date") %>% 
+  left_join(metric_3_ahead %>% select(date, three_ahead), by="date")
+
+metric_final %>%
+  gather(type, value, -date) %>% 
+  ggplot() +
+  aes(x=date, y=value, color=type) +
+  geom_line()
