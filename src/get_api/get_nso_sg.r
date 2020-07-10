0051e157104cde61bc5b4de04604ddf34a0f9d00
dhopp1 <daniel.hopp1@gmail.com>
2020-07-10 11:48:03 +0200

    Fixes bug with singapore ports JSON

--- src/get_api/get_nso_sg.r
+++ src/get_api/get_nso_sg.r
@@ -3,7 +3,7 @@ get_nso_sg <- function (url, catalog, g, start_date, end_date) {
   tmp <- gen_tmp(vars, start_date, end_date)
   
   status <- tryCatch({
-    rawdata <- fromJSON(url)
+      rawdata <- fromJSON(url)
     TRUE },
     error = function(e) { FALSE })
   if (status) {
@@ -17,5 +17,27 @@ get_nso_sg <- function (url, catalog, g, start_date, end_date) {
     tmp[starti:(starti + nrow(data) - 1), 2] <- data$value
     
     return(tmp %>% select(-1))
+  } else {
+    # if ssh certificate isn't working
+    status2 <- tryCatch({
+      suppressMessages(
+        system(str_interp("wget --no-check-certificate ${url} --output-document=tmp.json"))
+      )
+      rawdata <- fromJSON("tmp.json")
+      system("rm tmp.json") 
+      TRUE },
+      error = function(e) {FALSE})
+    if (status2) {
+      data <- rawdata$Level1d %>%
+        as_tibble() %>%
+        mutate(date = as.Date(paste(month, "01", sep = "-"), format = "%Y-%m-%d")) %>%
+        filter(date >= start_date) %>%
+        mutate(value = as.numeric(value)) %>%
+        arrange(date)
+      starti <- which(grepl(pull(data[1, "date"]), tmp$date))
+      tmp[starti:(starti + nrow(data) - 1), 2] <- data$value
+      
+      return(tmp %>% select(-1)) 
+    }
   }
 }
