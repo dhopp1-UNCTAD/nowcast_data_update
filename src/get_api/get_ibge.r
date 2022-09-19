48c366e12aaecf3aac3ec32eab6ea29de31e9301
dhopp1 <daniel.hopp1@gmail.com>
2022-09-19 16:09:47 +0200

    Updating ibge brazil

--- src/get_api/get_ibge.r
+++ src/get_api/get_ibge.r
@@ -1,34 +1,34 @@
 get_ibge <- function (url, catalog, g, start_date, end_date) {
   vars <- gen_vars(catalog, g)
   tmp <- gen_tmp(vars, start_date, end_date)
-  if (vars$code[1] == "is_br") {
-    sheet_num <- 2
-    first_date <- as.Date("2011-01-01")
-  } else {
-    sheet_num <- 1
-    first_date <- as.Date("2002-01-01")
-  }
+  
+  months <- seq(as.Date(start_date), as.Date(end_date), by = "month") %>%
+    sapply(function (x) paste0(substr(x, 1, 4), substr(x, 6, 7))) %>%
+    unname() %>%
+    paste(collapse="|")
+  url <- str_replace(url,"MONTHS", months)
   
   status <- tryCatch({
-    tmps <- tempfile()
-    download.file(url, tmps, quiet = T, mode = "wb")
-    rawdata <- read_excel(path = tmps, skip = 2, col_names = T, sheet = sheet_num)
-    unlink(tmps)
+    rawdata <- fromJSON(url)
     TRUE },
     error = function(e) {
       FALSE })
   if (status) {
-    data <- rawdata %>%
-      slice(1, 3) %>%
-      t(.) %>%
-      as_tibble(.) %>%
-      slice(-1)
-    colnames(data) <- c("date", "value")
+    data <- rawdata$resultados[[1]][[2]] %>% 
+      data.frame() %>%
+      select(starts_with("serie")) %>%
+      t() %>%
+      data.frame()
+    data$date <- rownames(data)
+    rownames(data) <- 1:nrow(data)
     data <- data %>%
-      mutate(date = seq.Date(from = first_date, by = "month", length.out = nrow(.))) %>%
-      mutate(value = as.numeric(value))
-    starti <- which(grepl(pull(data[1, "date"]), tmp$date))
-    tmp[starti:(starti + nrow(data) - 1), 2] <- data$value
+      mutate(date = as.Date(paste0(substr(date, 7, 10), "-", substr(date, 11, 12), "-01"))) %>%
+      select(date, X1) %>%
+      rename(!!vars$code[1] := X1)
+    
+    tmp <- tmp %>%
+      select(date) %>%
+      left_join(data, by="date")
     
     return(tmp %>% select(-1))
   }
